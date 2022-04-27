5dee798f3a0a77367e56e926ff2a1da72e9216db
ljuglaret <ljuglaret@gmail.com>
2022-04-27 22:39:40 +0200

    correction et generation d'une grille aléatoire

--- nonogramme/src/Jeu.js
+++ nonogramme/src/Jeu.js
@@ -3,12 +3,32 @@ import "./App.css";
 const dimension = 4;
 //attention , il s'agit d'un exemple de présentation, 
 //il peut y avoir des incohérences
-const indications = [[1,2],[0],[3,2],[2],[2],[1,2],[0],[1]]
+const indicsLigne = []//[[1,2],[0],[3,2],[2]]
+const indicsColonne = []//[[2],[1,2],[0],[1]]
+
 const caseInitiale = {color:"white", click : false};
 const grille = []
 const grilleAttendue = []
 
 export default function Jeu()  {
+
+  // Mettre les indications dans une autre couleur lorsque 
+  // une ligne/colonne est complète
+  // === si la ligne/colonne n'a aucune case dont la color est white
+
+  function ligneColComplete(){
+  //  genereGrille
+    for (let i = 0 ; i < dimension ; i++){
+      //let hasMagenicVendor = vendors.some( vendor => vendor['Name'] === 'Magenic' )
+       if (!grille[i].some(ligne => ligne['color'] === caseInitiale.color)){
+         console.log(i + "est pleine")
+         document.getElementsByClassName("indicationsi")[i].className = "indicationsf"
+       }
+      }
+  }
+
+  // Si erreur ne pas mettre en rouge mais en gris 
+  // et à ajouter ce nombre à la vue
 
   function genereGrille(){
     genereationAlea()
@@ -41,7 +61,7 @@ export default function Jeu()  {
 
   function genereationAlea(){
        //remplis aleatoirement une grille
-       const colors = ["btn btn-gray","btn btn-green"]
+       const colors = ["gridCell gridCell-gray","gridCell gridCell-green"]
        for (let i = 0 ; i < dimension ; i++){
          const ligneAlea = []
          for (let j = 0 ; j < dimension ; j ++){ 
@@ -50,26 +70,65 @@ export default function Jeu()  {
            }
          grilleAttendue[i] = ligneAlea
        }
+       genereIndications()
   }
-  function correction(){
- 
-//compte les cases grisées sur la grille attendue
-    for (let i = 0 ; i < grilleAttendue.length ; i++){
-      //let comptelignei = 0;
-      for (let j = 0 ; j < grilleAttendue.length ; j ++){
-        
-        //indications[]
+
+function genereIndications(){
+  console.log(" attendue : ")
+    console.log(grilleAttendue);
+
+  //compte les cases grisées sur la grille attendue
+for (let i = 0 ; i < dimension ; i++){
+  let temp = [], cpt = 0 , k = 0  
+      for (let j = 0 ; j < dimension ; j ++){
+        if(grilleAttendue[i][j].color === 'gridCell gridCell-green' && j === dimension - 1){
+          cpt++;
+          temp[k] = cpt;
+        }
+        else if(grilleAttendue[i][j].color === 'gridCell gridCell-green'){
+          cpt++;
+        }
+      
+        else if(grilleAttendue[i][j].color === 'gridCell gridCell-gray' && cpt !==0){
+          temp[k] = cpt;
+          k++;
+          cpt = 0 ;
+        }
+        indicsLigne[i] = temp
     }
   }
- 
+  for (let j = 0 ; j < dimension ; j++){
+    let temp = [], cpt = 0 , k = 0  
+        for (let i = 0 ; i < dimension ; i ++){
+          if(grilleAttendue[i][j].color === 'gridCell gridCell-green' && i === dimension - 1){
+            cpt++;
+            temp[k] = cpt;
+          }
+          else if(grilleAttendue[i][j].color === 'gridCell gridCell-green'){
+            cpt++;
+          }
+        
+          else if(grilleAttendue[i][j].color === 'gridCell gridCell-gray' && cpt !==0){
+            temp[k] = cpt;
+            k++;
+            cpt = 0 ;
+          }
+          indicsColonne[j] = temp
+      }
+    }
+    console.log("indic lignes : "+indicsLigne)
+    console.log("indic colonnes : "+indicsColonne)
+}
 
-  for (let i = 0 ; i < grilleAttendue.length ; i++){
-     for (let j = 0 ; j < grilleAttendue.length ; j ++){
+function correction(i,j){
+//comparaisons réponses données et réponses attendues
+  
      if(grille[i][j].color !== grilleAttendue[i][j].color){
-       console.log("erreur en " + i +"-"+ j)
+      grille[i][j].color = "gridCell gridCell-red"
+      const caseNumCase = document.getElementById("case"+(i*dimension+j))
+      caseNumCase.className = grille[i][j].color
      }
-   }
- }
+ 
 }
 
   function changeColor(){
@@ -88,12 +147,12 @@ export default function Jeu()  {
     const newColorClassName = "gridCell gridCell-"+btn.className.split(" ")[1].split("-")[1]; 
     caseNumCase.className = newColorClassName
     caseNumCase.disabled = true;//
-    grille[i-1][j-1] ={color:btn.className,click:true}
-    correction()
-
+    grille[i][j] ={color: newColorClassName,click:true}
+    console.log("couleur choisie : " + newColorClassName)
+    correction(i,j)
+    ligneColComplete()
     console.log(grille);
-    console.log(grilleAttendue);
-
+    
   }
 
   function defBtnCase(btnCase,i,j){
@@ -102,19 +161,19 @@ export default function Jeu()  {
 
     }
     else if (i === 0){
-      btnCase.id = "indications"
+      btnCase.className = "indicationsj"
       btnCase.disabled = true
-      btnCase.innerText = indications[j-1]
+      btnCase.innerText = indicsColonne[j-1]
     }
     else if (j === 0){
-      btnCase.id = "indications"
+      btnCase.className = "indicationsi"
       btnCase.disabled = true
-      btnCase.innerText = indications[4+ (i-1)]
+      btnCase.innerText = indicsLigne[i-1]
     }
     else{
-      btnCase.id = "case" + (i*dimension+j);
+      btnCase.id = "case" + ((i-1)*dimension+(j-1));
       btnCase.className = "gridCell gridCell-white";
-      btnCase.onclick = () => {changeState(i,j);}
+      btnCase.onclick = () => {changeState(i-1,j-1);}
     }
   }
 
